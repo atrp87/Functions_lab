15afe613c0b81d162d3a4492b2de40b775b97de9
atrp87 <drewpeattie@hotmail.com>
2020-02-18 21:15:06 +0000

    functions_lab

---
+++ start_point/ruby_functions_practice.rb
@@ -0,0 +1,32 @@
+def return_10()
+  return 10
+end
+
+def add(num_1, num_2)
+  return num_1.to_i() + num_2.to_i()
+end
+
+
+def subtract (num_1, num_2)
+  return num_1.to_i() - num_2.to_i()
+end
+
+def divide (num_1, num_2)
+  return num_1.to_i() / num_2.to_i()
+end
+
+def multiply (num_1, num_2)
+  return num_1.to_i() * num_2.to_i()
+end
+
+def length_of_string()
+  return "A string of length 21".length()
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def string_as_number(num_1, num_2)
+  return num_1.to_i() + num_2.to_i()
+end
